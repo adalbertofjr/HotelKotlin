fdad11e21e3efc95a721fca39e848b23998e1151
Adalberto Fernandes Jr <adalberto.fernandesjr@gmail.com>
2019-04-06 14:18:26 -0300

    task: Adding layout to tablet's.

--- app/src/main/java/br/com/adalbertofjr/hotelkotlin/HotelActivity.kt
+++ app/src/main/java/br/com/adalbertofjr/hotelkotlin/HotelActivity.kt
@@ -4,17 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class HotelActivity : AppCompatActivity(), HotelListFragment.OnHotelClickListener {
-
-    override fun onHotelClick(hotel: Hotel) {
-        showDetailsActivity(hotel.id)
-    }
-
-    private fun showDetailsActivity(hotelId: Long) {
-        HotelDetailsActivity.open(this, hotelId)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hotel)
+    }
+
+    override fun onHotelClick(hotel: Hotel) {
+        if (isTablet()) {
+            showDetailsFragment(hotel.id)
+        } else {
+            showDetailsActivity(hotel.id)
+        }
+    }
+
+    private fun showDetailsFragment(hotelId: Long) {
+        val fragment = HotelDetailsFragment.newInstance(hotelId)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.details, fragment, HotelDetailsFragment.TAG_DETAILS)
+            .commit()
+    }
+
+    private fun isTablet(): Boolean = resources.getBoolean(R.bool.tablet)
+
+    private fun showDetailsActivity(hotelId: Long) {
+        HotelDetailsActivity.open(this, hotelId)
     }
 }
