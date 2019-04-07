8d6d1e75d91753b0c69d6c7f95911b5aa569f962
Adalberto Fernandes Jr <adalberto.fernandesjr@gmail.com>
2019-04-07 13:52:05 -0300

    task: Added multiple selection to delete items.
    Project: Hotel Kotlin

--- app/src/main/java/br/com/adalbertofjr/hotelkotlin/HotelListView.kt
+++ app/src/main/java/br/com/adalbertofjr/hotelkotlin/HotelListView.kt
@@ -1,6 +1,10 @@
 package br.com.adalbertofjr.hotelkotlin
 
 interface HotelListView {
-    fun showHotel(hotels: List<Hotel>)
+    fun showHotels(hotels: List<Hotel>)
     fun showHotelDetails(hotel: Hotel)
+    fun showDeleteMode()
+    fun hideDeleteMode()
+    fun showSelectedHotels(hotels: List<Hotel>)
+    fun updateSelectionCountText(count: Int)
 }
